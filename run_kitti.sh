d91dbcdc9c99c1f6c7f211536df541c97195f4e8
Lishunkai <worldprc@qq.com>
2018-03-25 17:38:55 +0800

    update

--- run_kitti.sh
+++ run_kitti.sh
@@ -1,4 +1,4 @@
-/home/kaiser/SLAM/orb_slam2/Examples/Monocular/mono_kitti \
-/home/kaiser/SLAM/orb_slam2/Vocabulary/ORBvoc.txt \
-/home/kaiser/SLAM/orb_slam2/Examples/Monocular/KITTI00-02.yaml \
+/home/kaiser/SLAM/SLAM_design_NKU/Examples/Monocular/mono_kitti \
+/home/kaiser/SLAM/SLAM_design_NKU/Vocabulary/ORBvoc.txt \
+/home/kaiser/SLAM/SLAM_design_NKU/Examples/Monocular/KITTI00-02.yaml \
 /home/kaiser/datasets/KITTI_dataset/sequences/00 \
