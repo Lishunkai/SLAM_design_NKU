024e1096c020fae7d9bc43d334c3a8c8284eff88
Lishunkai <worldprc@qq.com>
2018-03-25 17:34:46 +0800

    update

---
+++ Examples/Monocular/run_kitti.sh
@@ -0,0 +1,1 @@
+gnome-terminal -x bash -c "/home/kaiser/SLAM/orb_slam2/Examples/Monocular/mono_kitti /home/kaiser/SLAM/orb_slam2/Vocabulary/ORBvoc.txt /home/kaiser/SLAM/orb_slam2/Examples/Monocular/KITTI00-02.yaml /home/kaiser/datasets/KITTI_dataset/sequences/00" 
---
+++ run.sh
@@ -0,0 +1,7 @@
+/home/kaiser/SLAM/ORB_SLAM2_working/Examples/Monocular/mono_video \
+/home/kaiser/SLAM/ORB_SLAM2_working/Vocabulary/ORBvoc.bin \
+/home/kaiser/SLAM/ORB_SLAM2_working/parameter/monocular.yaml \
+/home/kaiser/SLAM/Datasets/orb_slam_data/09-04-08_22_51.mov \
+/home/kaiser/SLAM/Datasets/orb_slam_data/09-04-08_22_51.canII \
+-1
+
