bb0ba1d60dab55813e6948959b5c411a46547a0b
Lishunkai <worldprc@qq.com>
2018-03-29 16:48:38 +0800

    update run_kitti.sh

--- run_kitti.sh
+++ run_kitti.sh
@@ -1,4 +1,4 @@
 /home/kaiser/SLAM/SLAM_design_NKU/Examples/Monocular/mono_kitti \
 /home/kaiser/SLAM/SLAM_design_NKU/Vocabulary/ORBvoc.txt \
 /home/kaiser/SLAM/SLAM_design_NKU/Examples/Monocular/KITTI00-02.yaml \
-/home/kaiser/datasets/KITTI_dataset/sequences/00 \
+/home/kaiser/datasets/KITTI/sequences/00 \
