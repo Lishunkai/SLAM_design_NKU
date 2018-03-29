431f3d7cdf9c76d8ae7b182daa3608bfc02c33db
Lishunkai <worldprc@qq.com>
2018-03-29 16:50:54 +0800

    add vocabulary

--- run_kitti.sh
+++ run_kitti.sh
@@ -1,4 +1,4 @@
 /home/kaiser/SLAM/SLAM_design_NKU/Examples/Monocular/mono_kitti \
-/home/kaiser/SLAM/SLAM_design_NKU/Vocabulary/ORBvoc.txt \
+/home/kaiser/SLAM/SLAM_design_NKU/Vocabulary/TrainedVocabulary.bin \
 /home/kaiser/SLAM/SLAM_design_NKU/Examples/Monocular/KITTI00-02.yaml \
 /home/kaiser/datasets/KITTI/sequences/00 \
